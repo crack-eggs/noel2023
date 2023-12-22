c93b2d191579ddec14d09c2a0c770e6df00fc782
Duy Mai <Duy.maid@one-line.com>
2023-12-22 08:54:00 +0700

    2

--- lib/main.dart
+++ lib/main.dart
@@ -65,7 +65,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primaryColor: primaryColor,
         ),
-        // initialRoute: '/',
+        initialRoute: '/',
         // initialRoute: '/login?match_id=${const Uuid().v4()}',
         home: !isWebMobile ? Container() : const HomePage(),
 
